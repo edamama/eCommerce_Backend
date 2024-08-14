a21647ed1a0b4cee8453d3a772af711d6f0af420
Tyler Payne <169082073+edamama@users.noreply.github.com>
2024-08-13 20:33:31 -0500

    Add files via upload

--- models/index.js
+++ models/index.js
@@ -17,7 +17,7 @@ Category.hasMany(Product,{
 
 // Products belongToMany Tags (through ProductTag)
 
-Product.belongToMany(Tag,{
+Product.belongsToMany(Tag,{
 
   through: ProductTag,
   as:"productTag_products",
